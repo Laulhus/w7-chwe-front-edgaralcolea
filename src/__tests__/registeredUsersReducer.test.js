a4f69ddf25c3aeec064ba5290326fb6a9086b77c
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-27 15:19:12 +0100

    Created rootReducer

---
+++ src/__tests__/registeredUsersReducer.test.js
@@ -0,0 +1,5 @@
+describe("Given a registeredUsersReducer function", () => {
+  describe("When it receives a createUserAction", () => {
+    test("Then it should return current users adding the user in the received action", () => {});
+  });
+});
