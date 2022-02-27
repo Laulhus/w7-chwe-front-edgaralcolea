628ec51646a82396401264b4363810974f099018
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-27 17:19:20 +0100

    Installed msw package and created mock handlers

---
+++ src/mocks/handlers.js
@@ -0,0 +1,22 @@
+import { rest } from "msw";
+
+const handlers = [
+  rest.post(
+    `${process.env.REACT_APP_API_URL}users/register`,
+    (req, res, ctx) => {
+      res(
+        ctx.status(201),
+        ctx.json({
+          name: "Testman",
+          lastName: "McTest",
+          userName: "Testoman",
+          password: "hola",
+          age: 30,
+          city: "Testingvania",
+        })
+      );
+    }
+  ),
+];
+
+export default handlers;
