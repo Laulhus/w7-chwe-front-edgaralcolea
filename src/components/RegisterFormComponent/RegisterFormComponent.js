d261132a6aa46668b96c701096acdf8867f93ecf
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-27 12:12:46 +0100

    Added button and linked input values with data object

--- src/components/RegisterFormComponent/RegisterFormComponent.js
+++ src/components/RegisterFormComponent/RegisterFormComponent.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 const RegisterFormComponent = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
   };
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+  const blankForm = {
+    userName: "",
+    password: "",
+    name: "",
+    lastName: "",
+    age: null,
+    city: "",
+    profile: null,
+  };
+
+  const [formData, setFormData] = useState(blankForm);
+
+  const isFilled =
+    formData.userName !== "" &&
+    formData.password !== "" &&
+    formData.name !== "" &&
+    formData.lastName !== "" &&
+    formData.age !== null &&
+    formData.city !== "";
 
   return (
     <form
@@ -18,43 +42,83 @@ const RegisterFormComponent = () => {
           <label htmlFor="userName" title="userName">
             Username:
           </label>
-          <input type="text" id="userName" name="userName" required />
+          <input
+            type="text"
+            id="userName"
+            name="userName"
+            onChange={handleChange}
+            required
+          />
         </li>
         <li>
           <label htmlFor="password" title="password">
             Password:
           </label>
-          <input type="text" id="password" name="password" required />
+          <input
+            type="text"
+            id="password"
+            name="password"
+            onChange={handleChange}
+            required
+          />
         </li>
         <li>
           <label htmlFor="name" title="name">
             Name:
           </label>
-          <input type="text" id="name" name="name" required />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={handleChange}
+            required
+          />
         </li>
         <li>
           <label htmlFor="lastName" title="lastName">
             Last Name:
           </label>
-          <input type="text" id="lastName" name="lastName" required />
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            onChange={handleChange}
+            required
+          />
         </li>
         <li>
           <label htmlFor="age" title="age">
             Age:
           </label>
-          <input type="number" id="age" name="age" required />
+          <input
+            type="number"
+            id="age"
+            name="age"
+            onChange={handleChange}
+            required
+          />
         </li>
         <li>
           <label htmlFor="city" title="city">
             City:
           </label>
-          <input type="text" id="city" name="city" />
+          <input type="text" id="city" name="city" onChange={handleChange} />
         </li>
         <li>
           <label htmlFor="picture" title="picture">
             Profile picture:
           </label>
-          <input type="file" id="picture" name="picture" />
+          <input
+            type="file"
+            id="picture"
+            name="picture"
+            onChange={handleChange}
+          />
+        </li>
+        <li>
+          <button type="submit" disabled={!isFilled}>
+            Register
+          </button>
         </li>
       </ul>
     </form>
