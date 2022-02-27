64052673f302a0d2195d7a227966bc84810039a3
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-27 21:49:48 +0100

    Removed unused var Router in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Navigate, Route, Router, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 
 function App() {
